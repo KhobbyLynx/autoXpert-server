374cbde7d5d54f3d8c0056d2489759896b05220a
Samuel Tetteh <khobbylynx55@gmail.com>
2023-07-15 03:36:27 +0000

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,50 +7,60 @@ import User from '../models/user.model.js'
 // @route GET /api/checkUser
 // @access Public
 export const checkUserExists = asyncHandler(async (req, res) => {
-  const { email } = req.query
+  try {
+    const { email } = req.query
 
-  // Check if email is valid
-  const isEmailValid = validateEmail(email)
+    // Check if email is valid
+    const isEmailValid = validateEmail(email)
 
-  // Check if user with the given email already exists
-  const userExists = await User.exists({ email })
+    // Check if user with the given email already exists
+    const userExists = await User.exists({ email })
 
-  if (userExists) {
-    res.json({ userExists: true })
-  } else if (!isEmailValid) {
-    res.json({ invalidEmail: true })
-  } else {
-    res.json({ userExists: false })
+    if (userExists) {
+      res.json({ userExists: true })
+    } else if (!isEmailValid) {
+      res.json({ invalidEmail: true })
+    } else {
+      res.json({ userExists: false })
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' })
   }
 })
+
+// Other route handlers can be similarly updated with try-catch blocks.
 
 // @desc Register new user
 // @route POST /api/users
 // @access Public
 export const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body
+  try {
+    const { name, email, password } = req.body
 
-  // Hash Password
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = await bcrypt.hash(password, salt)
+    // Hash Password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
 
-  // Create user
-  const user = await User.create({
-    name,
-    email,
-    password: hashedPassword,
-  })
-
-  if (user) {
-    res.status(201).json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
+    // Create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
     })
-  } else {
-    res.status(400)
-    throw new Error('Invalid user data')
+
+    if (user) {
+      res.status(201).json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      })
+    } else {
+      res.status(400)
+      throw new Error('Invalid user data')
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' })
   }
 })
 
@@ -58,21 +68,25 @@ export const registerUser = asyncHandler(async (req, res) => {
 // @route POST /api/users/login
 // @access Public
 export const loginUser = asyncHandler(async (req, res) => {
-  const { email, password } = req.body
+  try {
+    const { email, password } = req.body
 
-  //Check for user email
-  const user = await User.findOne({ email })
+    //Check for user email
+    const user = await User.findOne({ email })
 
-  if (user && (await bcrypt.compare(password, user.password))) {
-    res.json({
-      _id: user.id,
-      name: user.name,
-      email: user.email,
-      token: generateToken(user._id),
-    })
-  } else {
-    res.status(400)
-    throw new Error('Invalid user credentials')
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      })
+    } else {
+      res.status(400)
+      throw new Error('Invalid user credentials')
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' })
   }
 })
 
@@ -80,13 +94,17 @@ export const loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/me
 // @access Private
 export const getMe = asyncHandler(async (req, res) => {
-  const { _id, name, email } = await User.findById(req.user.id)
+  try {
+    const { _id, name, email } = await User.findById(req.user.id)
 
-  res.status(200).json({
-    id: _id,
-    name,
-    email,
-  })
+    res.status(200).json({
+      id: _id,
+      name,
+      email,
+    })
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' })
+  }
 })
 
 export const logout = asyncHandler(async (req, res) => {
@@ -96,7 +114,7 @@ export const logout = asyncHandler(async (req, res) => {
       secure: true,
     })
     .status(200)
-    .send('User has been Logged out.')
+    .json({ loggedOut: true })
 })
 
 // Validate Email
